b8d32dd9404cac9da3a2e73ab352af1f44d0e4c7
kwonssshyeon <kwonsh4396@gmail.com>
2023-07-18 21:45:03 +0900

    [feat-#1]login 조건 수정

--- src/main/java/com/example/news_backend/repository/UserRepository.java
+++ src/main/java/com/example/news_backend/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.news_backend.repository;
 import com.example.news_backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByIdAndPassword(String id, String password);
 }
