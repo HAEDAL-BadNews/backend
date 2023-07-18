0d599d02018de735e3edf02fed8e68f8ba4f307b
Yongmin Kwon <rnjs5540@gmail.com>
2023-07-18 14:52:28 +0900

    test

--- src/main/java/com/example/news_backend/NewsBackendApplication.java
+++ src/main/java/com/example/news_backend/NewsBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewsBackendApplication {
-	// test
+
 	public static void main(String[] args) {
 		SpringApplication.run(NewsBackendApplication.class, args);
 	}
