2ce9acf885554723a03a0335d283722f296e3573
kwonssshyeon <kwonsh4396@gmail.com>
2023-07-19 01:56:10 +0900

    [feat-#2]엔티티 작성 완료

---
+++ src/main/java/com/example/news_backend/domain/Article.java
@@ -0,0 +1,42 @@
+package com.example.news_backend.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userId;
+    private String title;
+    private String context;
+    private String url;
+    private String author;
+    private LocalDate article_date;
+    private boolean scrap;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    @ElementCollection(fetch = FetchType.LAZY)
+    private Set<String> keyword = new HashSet<>();
+    private LocalDate now;
+
+}
+
+
+enum Category{
+    sport,
+    economy,
+    social,
+    IT
+}
