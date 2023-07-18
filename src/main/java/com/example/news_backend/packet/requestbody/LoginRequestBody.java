6a1690a47bde1edc36ea02f3e06365cf6287794d
kwonssshyeon <kwonsh4396@gmail.com>
2023-07-18 21:37:32 +0900

    [feat-#2]connect H2 database

--- src/main/java/com/example/news_backend/packet/requestbody/LoginRequestBody.java
+++ src/main/java/com/example/news_backend/packet/requestbody/LoginRequestBody.java
@@ -1,12 +1,16 @@
 package com.example.news_backend.packet.requestbody;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestBody {
-    String id;
-    String password;
+    private String id;
+    private String password;
 }
