47df7ed95849c2125370a78b9617c0a70b5345e9
kwonssshyeon <kwonsh4396@gmail.com>
2023-08-16 22:28:17 +0900

    [feat-#3]RDS 연결 설정

--- src/main/java/com/example/news_backend/domain/User.java
+++ src/main/java/com/example/news_backend/domain/User.java
@@ -3,6 +3,7 @@ package com.example.news_backend.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "USER_TABLE")
 public class User {
     @Id
     private String id;
